aa40bb6ea73be3a21a14697df9af7d3e4acaa96e
rezaagust <reza.2020@smk-strada.sch.id>
2022-01-24 08:12:10 +0700

    tugaspbo

--- angkaGanjilGenap.js
+++ angkaGanjilGenap.js
@@ -7,3 +7,17 @@ for (let i = 1; i <= 100; i+= 3) {
 }
 
 console.log("###")
+
+
+for (let i = 50; i <= 200; i+= 5) {
+    if(i % 3 === 0) {
+        console.log(i + "- hasil dibagi 3")
+    }
+}
+
+
+for (let i = 100; i <= 200; i+= 7) {
+    if(i % 8 === 0) {
+        console.log(i + "- hasil dibagi 8")
+    }
+}
